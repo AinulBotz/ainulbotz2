46003b3444c8424364c5e5e581d66605accbfaad
AinulBotz <107542885+AinulBotz@users.noreply.github.com>
2022-07-03 20:44:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -56,8 +56,8 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = sticker_name
-global.author = sticker_author
+global.packname = '©Ainul'
+global.author = 'Di Buat Oleh AinulBotz'
 global.wm = '©AinulBotz'
 
 global.multiplier = 1000 // The higher, The harder levelup
